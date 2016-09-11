d97e44f1885dac81d0106e7ec865dc531856449c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-11 09:22:07 +0200

    start basics.

---
+++ basics.v
@@ -0,0 +1,37 @@
+
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Compute (next_weekday friday).
+
+Compute (next_weekday (next_weekday saturday)).
+
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+
+Proof. simpl. reflexivity. Qed.
+
+
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+
+(** continue here https://www.cis.upenn.edu/~bcpierce/sf/current/Basics.html#next_weekday **)
